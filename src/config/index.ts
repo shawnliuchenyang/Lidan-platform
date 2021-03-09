0d894b06d623149a14b09f3061456dbaa7687844
shawnliuchenyang <shawnliuchenyang@didiglobal.com>
2021-03-09 15:43:17 +0800

    1

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 
 // @ts-ignore
-export const baseURL = 'http://47.108.139.116';
-// export const baseURL = 'api/meet';
+// export const baseURL = 'http://47.108.139.116';
+export const baseURL = 'https://mcpopeye.com';
 
 export const cityURL = `/api/tangram/kf/tangram/third/city/list`
 export const cityGroupURL = "/api/tangram/kf/tangram/third/city_group"
